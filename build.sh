3a551cde3a1dd89ace5bb5da61c7f09d7f65d961
sebastianharko <sebastian.harko@lightbend.com>
2018-06-24 19:28:56 -0400

    add version 9.5 and 9.6

--- build.sh
+++ build.sh
@@ -6,9 +6,19 @@ cd ..
 cd 10.4
 docker build -t postgres104 . 
 cd ..
+cd 9.5
+docker build -t postgres95 .
+cd ..
+cd 9.6
+docker build -t postgres96 .
 
 docker tag postgres94 sebastianharko/postgres94
+docker tag postgres95 sebastianharko/postgres95
+docker tag postgres96 sebastianharko/postgres96
 docker tag postgres104 sebastianharko/postgres104
+
 docker push sebastianharko/postgres94
+docker push sebastianharko/postgres95
+docker push sebastianharko/postgres96
 docker push sebastianharko/postgres104 
 
